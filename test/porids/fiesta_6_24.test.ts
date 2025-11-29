b9c0479eb2f017283bdc0ec4c02ccd175e51abdd
Lucas <lucas@dex360.net>
2025-11-29 11:06:51 -0300

    corrigiendo sagrado corazon

--- test/porids/fiesta_6_24.test.ts
+++ test/porids/fiesta_6_24.test.ts
@@ -1,8 +1,8 @@
 import { traerContemplacionesSemana } from '../../lib/traerContemplacionesSemanaPorIds';
 
 describe('Fiesta fija 6-24', () => {
-  const idsEsperados = [11221];
-  const years = [2025];
+  const idsEsperados = [78774];
+  const years = [2025,2026];
   for (const year of years) {
     describe(`Año ${year}`, () => {
       const fecha = new Date(Date.UTC(year, 5, 24));
